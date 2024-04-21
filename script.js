d71b729f7f9d833429fd53fd9b35dc96adba6b85
krishannt <krishanntsaini@gmail.com>
2024-04-21 23:22:02 +0530

    Your commit message

---
+++ script.js
@@ -0,0 +1,82 @@
+
+let boxes = document.querySelectorAll(".box");
+let msgcontainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+let newGameBtn = document.querySelector("#new-btn"); 
+let resetBtn = document.querySelector("#reset-btn"); 
+
+let turnO = true;
+
+
+const winPattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgcontainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (box.innerText === "") {
+            if (turnO) {
+                box.innerText = "X";
+                turnO = false;
+            } else {
+                box.innerText = "O";
+                turnO = true;
+            }
+            box.disabled = true;
+            checkWinner(); 
+        }
+    });
+});
+
+ const disableBoxes =() => {
+   
+    for(let box of boxes){
+        box.disabled = true;
+    }
+ };
+ const enableBoxes =() => {
+   
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText ="";
+    }
+ };
+
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, the winner is ${winner}`;
+    msgcontainer.classList.remove("hide");
+    disableBoxes();
+}
+
+
+const checkWinner = () => {
+    for (let pattern of winPattern) {
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if (pos1val !== "" && pos2val !== "" && pos3val !== "") {
+            if (pos1val === pos2val && pos2val === pos3val) {
+                showWinner(pos1val);
+            }
+        }
+    }
+};
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
+
